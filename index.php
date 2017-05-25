ed066d5471143f14b1778079b23be15809744a38
Joris Rietveld <jorisrietveld@gmail.com>
2017-05-25 20:07:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Created: 25-05-2017 15:35
+ * Licence: GNU General Public licence version 3 <https://www.gnu.org/licenses/quick-guide-gplv3.html>
+ */
+declare(strict_types=1);
+
+?>
+<!doctype html>
+<title>INF1i</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #dc8100; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Inf1i WaterUp</h1>
+    <div>
+        <p>Sorry for the inconvenience but the website is still under construction. If you need to you can always <a href="mailto:jorisrietveld@gmail.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The Team</p>
+    </div>
+</article>
