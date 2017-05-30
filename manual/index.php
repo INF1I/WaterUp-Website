dd9a2ff0447dfb543603162a1dc7b166a81b32f8
alwintje <alwinkroesen@gmail.com>
2017-05-30 14:33:32 +0200

    Manual

--- manual/index.php
+++ manual/index.php
@@ -16,28 +16,107 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/Reset.css" rel="stylesheet" />
     <link href="css/Default.css" rel="stylesheet" />
     <title>User Manual</title>
 </head>
 <body>
     <section id="main" class="static">
         <div class="logo">
-            <h1 class="name">WaterUp</h1>
-            <video autoplay width="1200" height="873" >
+            <h1 class="name">WaterUp<span class="sub">User Manual</span></h1>
+            <video autoplay width="1150" height="700" > <!-- 1200x873 -->
                 <source src="logo.webm" />
             </video>
-            <!--            <img src="images/logo.gif" class="logo-image" />-->
-            <!--            --><?php //include "images/logo.png"; ?>
         </div>
     </section>
-    <section>
-        <div class="logo">
-            test
-        </div>
+    <ul class="main-menu">
+        <li>
+            <a href="#download">Download the WaterUp app</a>
+        </li>
+        <li>
+            <a href="#connect">Connect with the WaterUp plant pot</a>
+        </li>
+        <li>
+            <a href="#choose_plant">Choose your plant</a>
+        </li>
+    </ul>
+    <div class="menu-spacing"></div>
+    <section id="download">
+        <header>
+            <h2>Download the WaterUp app</h2>
+        </header>
+        <article>
+            <div class="left">
+                <h2>Android</h2>
+                <ul>
+                    <li>Open the 'Play Store'.</li>
+                    <li>In the search box at the top, type: 'WaterUp - app'</li>
+                    <li>Select the app 'WaterUp - app'</li>
+                    <li>Tap 'Install'</li>
+                    <li>After install, tap on 'Open' to open the app</li>
+                </ul>
+            </div>
+            <div class="right">
+                <h2>Apple</h2>
+                <ul>
+                    <li>Open the 'App Store'.</li>
+                    <li>In the search box at the top, type: 'WaterUp - app'</li>
+                    <li>Select the app 'WaterUp - app' and click on 'Get'</li>
+                    <li>Tap on 'Install' that appears in the same location as the previous get button</li>
+                    <li>After install, tap on 'Open' to open the app</li>
+                </ul>
+            </div>
+        </article>
     </section>
-    <section></section>
-    <section></section>
-    <section></section>
+    <section id="connect">
+        <header>
+            <h2>Connect with the WaterUp plant pot</h2>
+        </header>
+        <article>
+            <div class="left">
+                <ul>
+                    <li>Open the app you've downloaded.<br />
+                        If you haven't download it yet, follow the previous instruction: '<a href="#download">Download the app</a>'</li>
+                    <li>Tap the plus in top-right corner</li>
+                    <li>Take a photo of the QR-code. <br />
+                        You can find the QR-code on the first page after the cover of the User Manual which is included when you bought the WaterUp plant pot.</li>
+                    <li>When the message 'Connected' appears, the plant pot is added.</li>
+                </ul>
+            </div>
+            <div class="right">
+                <figure>
+                    <img src="http://chart.apis.google.com/chart?chs=200x200&cht=qr&chl=Example&choe=U%E2%80%8C%E2%80%8BTF-8&chld=M|0" alt="QR-code example"/>
+                    <figcaption>QR-code example</figcaption>
+                </figure>
+            </div>
+        </article>
+    </section>
+    <section id="choose_plant">
+        <header>
+            <h2>Choose your plant</h2>
+        </header>
+        <article>
+            <div class="left">
+                <ul>
+                    <li>Open the app you've downloaded.<br />
+                        If you haven't download it yet, follow the previous instruction: '<a href="#download">Download the app</a>'</li>
+                    <li>Tap the plus in top-right corner</li>
+                    <li>Take a photo of the QR-code. <br />
+                        You can find the QR-code on the first page after the cover of the User Manual which is included when you bought the plant pot.</li>
+                    <li>When the message 'Connected' appears, the plant pot is added.</li>
+                </ul>
+            </div>
+            <div class="right">
+                <figure>
+                    <img src="http://chart.apis.google.com/chart?chs=200x200&cht=qr&chl=Example&choe=U%E2%80%8C%E2%80%8BTF-8&chld=M|0" alt="QR-code example"/>
+                    <figcaption>QR-code example</figcaption>
+                </figure>
+            </div>
+        </article>
+    </section>
+    <section id="">
+
+    </section>
 <script src="js/Default.js"></script>
 </body>
 </html>
--- manual/js/Default.js
+++ manual/js/Default.js
@@ -3,17 +3,29 @@
  */
 
 
-var body, sections;
+var body, sections, menu, menuSpacing;
+var menuOffsetTop;
 var i;
 window.onload = function(){
     body = document.body || document.documentElement;
     sections = document.querySelectorAll("body section");
-
+    menu = document.getElementsByClassName("main-menu")[0];
+    menuSpacing = document.getElementsByClassName("menu-spacing")[0];
+    menuOffsetTop = menu.offsetTop;
 };
 
 
 window.onscroll = function() {
     var pageY = document.body.scrollTop || document.documentElement.scrollTop;
+    if(pageY > menuOffsetTop){
+        menu.style.position = "fixed";
+        menu.style.top = "0px";
+        menuSpacing.style.display = "block";
+    }else{
+        menu.style.position = "relative";
+        menu.style.top = "-50px";
+        menuSpacing.style.display = "none";
+    }
     for( i=0; i < sections.length; i++ ){
         var c = sections[i].getAttribute("class");
         // if(sections[i].offsetTop < pageY && (!c || c.indexOf("static") === -1)){
@@ -22,6 +34,18 @@ window.onscroll = function() {
 
     }
     console.log(pageY);
-    // console.log(sections[1].offsetTop);
+    console.log(menu.offsetTop);
 
 };
+
+// if ("onhashchange" in window) {
+//     alert("The browser supports the hashchange event!");
+// }
+//
+// function locationHashChanged() {
+//     if (location.hash === "#somecoolfeature") {
+//         somecoolfeature();
+//     }
+// }
+//
+// window.onhashchange = locationHashChanged;
