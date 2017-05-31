f121d4d649cdf34a6bc51d55f986efc62990da33
alwintje <alwinkroesen@gmail.com>
2017-05-31 22:18:54 +0200

    Manual canvas

--- manual/js/Explanations.js
+++ manual/js/Explanations.js
@@ -13,55 +13,23 @@ function Explanations(){
     img.onload = function(){
         ctx.drawImage(this,360,0);
 
-        c.onmouseover = function(){
-            ctx.beginPath();
-            ctx.moveTo(480,155);
-            ctx.lineTo(100,155);
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "#FFF";
-            // ctx.fontSize = "40px";
-            //
-            // ctx.strokeWidth("5")
-            ctx.stroke();
-            ctx.font = "16pt Century Gothic";
-            ctx.fillStyle = "#FFF";
-            ctx.fillText("Water percentage",100,145);
-
-            ctx.beginPath();
-            // 230
-            // 330
-            ctx.arc(720, 590, 120, Math.PI * 1, 2 * Math.PI, false);
-            ctx.arc(720, 670, 120, Math.PI * 2, 1 * Math.PI, false);
-            ctx.lineTo(600,590);
-            // ctx.fillStyle = 'rgba(255,255,255,0.5)';
-            ctx.fillStyle = 'rgba(0,0,0,0.6)';
-            ctx.fill();
-            ctx.lineWidth = 2;
-            // ctx.strokeStyle = "#A33";
-            ctx.stroke();
-            ctx.lineWidth = 2;
-            ctx.beginPath();
-            ctx.moveTo(720,630);//280
-            ctx.lineTo(1100,630);
-            ctx.stroke();
-
-            ctx.font = "16pt Century Gothic";
-            ctx.fillStyle = "#FFF";
-            ctx.fillText("WaterUp plant pot",905,620);
-        };
-        c.onmouseout = function () {
-            ctx.clearRect(0, 0, c.width, c.height);
-            ctx.drawImage(img,360,0);
-        }
         c.addEventListener('mousemove', function(e) {
+
             var mousePos = getMousePos(c, e);
             var x = mousePos.x;
             var y = mousePos.y;
-            // mousePos.x
-            // mousePos.y
-            // if(isInBlock(x,y, )){//
-            //
-            // }
+            ctx.clearRect(0, 0, c.width, c.height);
+            ctx.drawImage(img,360,0);
+
+            if(isInBlock(x,y, 564, 78, 648, 162)){
+                getCode(ctx).addWaterUpPlantPot();
+            }
+            if(isInBlock(x,y, 360, 150, 600, 450)){
+                getCode(ctx).waterPercentage();
+            }
+            if(isInBlock(x, y, 600, 460, 840, 800)){
+                getCode(ctx).waterUpPlantPot();
+            }
         }, false);
 
     };
@@ -86,4 +54,71 @@ function isInBlock(currX, currY, startX, startY, endX, endY){
         currX <= endX &&
         currY >= startY &&
         currY <= endY;
+}
+
+function getCode(ctx){
+    return {
+        waterPercentage: function(){
+
+            // Line to text
+            ctx.beginPath();
+            ctx.moveTo(480,155);
+            ctx.lineTo(100,155);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "#FFF";
+            ctx.stroke();
+
+            // Text
+            ctx.font = "16pt Century Gothic";
+            ctx.fillStyle = "#FFF";
+            ctx.fillText("Water percentage",100,145);
+        },
+        addWaterUpPlantPot: function(){
+
+            // Circle
+            ctx.beginPath();
+            ctx.arc(606, 120, 40, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(0,0,0,0.6)';
+            ctx.strokeStyle = "#FFF";
+            ctx.fill();
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            // Line to text
+            ctx.beginPath();
+            ctx.moveTo(646,120);//280
+            ctx.lineTo(1170,120);
+            ctx.stroke();
+
+            // Text
+            ctx.font = "16pt Century Gothic";
+            ctx.fillStyle = "#FFF";
+            ctx.fillText("Add a new WaterUp plant pot",854,110);
+        },
+        waterUpPlantPot: function () {
+
+            // Oval
+            ctx.beginPath();
+            ctx.arc(720, 590, 120, Math.PI * 1, 2 * Math.PI, false);
+            ctx.arc(720, 670, 120, Math.PI * 2, 1 * Math.PI, false);
+            ctx.lineTo(600,590);
+            ctx.fillStyle = 'rgba(0,0,0,0.6)';
+            ctx.strokeStyle = "#FFF";
+            ctx.fill();
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.lineWidth = 2;
+
+            // Line to text
+            ctx.beginPath();
+            ctx.moveTo(720,630);//280
+            ctx.lineTo(1100,630);
+            ctx.stroke();
+
+            // Text
+            ctx.font = "16pt Century Gothic";
+            ctx.fillStyle = "#FFF";
+            ctx.fillText("WaterUp plant pot",905,620);
+        }
+    };
 }
